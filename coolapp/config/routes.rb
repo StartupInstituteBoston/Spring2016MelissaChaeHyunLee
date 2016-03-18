5f505bf4261b64ab61eaa40d3da39357573f48a4
Melissa Lee <melissachlee@gmail.com>
2016-03-18 11:03:21 -0400

    added devise

--- coolapp/config/routes.rb
+++ coolapp/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'restaurants/index'
   resources :restaurants
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
